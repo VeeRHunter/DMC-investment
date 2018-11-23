c82b34b014f73fdf9a355d89c20f04cf0c23eb40
VeeR <VeeR>
2018-11-23 09:53:54 +0700

    add new splash screen. change finger image. change backgound from white to #2a363e. remove all html and functions from main page.

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -35,36 +35,13 @@ export class MainPage {
   public weekList = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
   public dailyList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 
-
-
-  public chartLabels = ['Gold', 'Silver', 'USD', 'AUD'];
-  public chartColors = ['#ffdf00', '#d3d3d3', '#85bb65', '#012169'];
-  public chartData = [100, 150, 130, 500];
-  public chartType = 'line';
-  public enableShowChat = false;
-
-  public lineChartData: Array<any> = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Gold', fill: false, lineTension: 0 },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Silver', fill: false, lineTension: 0 },
-    { data: [23, 38, 48, 89, 26, 67, 20], label: 'USD', fill: false, lineTension: 0 },
-    { data: [18, 48, 77, 9, 100, 27, 40], label: 'AUD', fill: false, lineTension: 0 }
-  ];
-  // public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-  public lineChartLabels: Array<any> = [1, 2, 3, 4, 5, 6, 7];
-  public lineChartOptions: any = {
-    responsive: true,
-    legend: {
-      display: true,
-      position: 'bottom',
-      fullWidth: true,
-    }
-  };
-  public lineChartColors: any[];
-  public lineChartLegend: boolean = true;
-  public lineChartType: string = 'line';
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController, public toastCtrl: ToastController, public apiserver: ServerProvider) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public loadingCtrl: LoadingController,
+    public toastCtrl: ToastController,
+    public apiserver: ServerProvider,
+  ) {
   }
 
   ionViewDidLoad() {
@@ -122,118 +99,6 @@ export class MainPage {
 
   changeToDecimal(inputData) {
     return parseFloat(inputData).toFixed(2);
-  }
-
-  liveFeedCall() {
-    console.log("liveFeedCall");
-  }
-
-  accessCode() {
-    console.log("accessCode");
-  }
-
-  clickBuy() {
-    console.log("clickBuy");
-    localStorage.setItem("tradeType", "buy");
-    this.navCtrl.push('TradeCenterPage');
-  }
-
-  clickSell() {
-    console.log("clickSell");
-    localStorage.setItem("tradeType", "sell");
-    this.navCtrl.push('TradeCenterPage');
-  }
-
-  gotoRealEstate() {
-    this.navCtrl.push('PendingPage');
-  }
-
-  gotoTransaction() {
-    localStorage.setItem("originalInvestment", this.orgInvestment);
-    this.navCtrl.push('TransactionPage');
-  }
-
-  public randomize(): void {
-    let _lineChartData: Array<any> = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = { data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label };
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-      }
-    }
-    this.lineChartData = _lineChartData;
-  }
-
-  // events
-  public chartClicked(e: any): void {
-    console.log(e);
-  }
-
-  public chartHovered(e: any): void {
-    console.log(e);
-  }
-
-  selectType(index) {
-    this.dataType = this.dataList[index];
-    if (this.dataList[index] === 'Monthly') {
-      // this.lineChartLabels = this.monthList;
-      this.setChartLineLabels(this.monthList);
-      this.setChatList(this.monthList.length);
-    }
-    else if (this.dataList[index] === 'Weekly') {
-      // this.lineChartLabels = this.weekList;
-      this.setChatList(this.weekList.length);
-      this.setChartLineLabels(this.weekList);
-    }
-    else if (this.dataList[index] === 'Daily') {
-      // this.lineChartLabels = this.weekList;
-      this.setChatList(this.dailyList.length);
-      this.setChartLineLabels(this.dailyList);
-    }
-  }
-
-  setChartLineLabels(lineValues) {
-    this.lineChartLabels.length = 0;
-    for (let list of lineValues) {
-      this.lineChartLabels.push(list);
-    }
-  }
-
-  setChatList(arrayNumber) {
-    this.lineChartData = new Array();
-    for (let list of this.chartLabels) {
-      let eachChat = { data: [], label: list, lineTension: 0 }
-      for (let i = 0; i < arrayNumber; i++) {
-        eachChat.data.push(Math.floor(Math.random() * 150) + 1);
-      }
-      console.log(eachChat);
-      this.lineChartData.push(eachChat);
-    }
-  }
-
-  setChatColors() {
-    this.lineChartColors = new Array();
-    this.lineChartColors.length = 0;
-    for (let list of this.chartColors) {
-      let eachColor = {
-        backgroundColor: '',
-        borderColor: '',
-        pointBackgroundColor: '',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: ''
-      }
-      eachColor.backgroundColor = 'transparent';
-      eachColor.borderColor = list;
-      eachColor.pointBackgroundColor = list;
-      eachColor.pointHoverBorderColor = list;
-      this.lineChartColors.push(eachColor);
-    }
-    // console.log(baseChart)
-    setTimeout(() => {
-      this.enableShowChat = true;
-    }, 1000);
-
   }
 
 }
